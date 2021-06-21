a05af3eacf5cfdd8b70b8c6d686fd775ae5e4879
AptYam <anikronjon.swe@gmail.com>
2021-06-21 21:44:06 +0600

    Register my models into admin file

--- classapp/admin.py
+++ classapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Post
+
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'category', 'published', 'image')
+    list_filter = ('author', 'category')
+    search_fields = ('title', 'body', 'author', 'category')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'published'
