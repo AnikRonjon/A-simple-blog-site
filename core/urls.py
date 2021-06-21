45d8163253212e5d36624db59d830ffee587246e
AptYam <anikronjon.swe@gmail.com>
2021-06-21 21:10:16 +0600

    Project Setup

---
+++ core/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+
+admin.site.site_title = "Yam's Site"
+admin.site.site_header = "Yam's Admin"
+admin.site.index_title = "Welcome To Yam's Administration"
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('classapp.urls', namespace='classapp')),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
