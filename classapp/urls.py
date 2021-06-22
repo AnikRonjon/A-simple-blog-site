e57cfc799fc9689da70a877b5cc545a139dfb973
AptYam <anikronjon.swe@gmail.com>
2021-06-22 19:43:52 +0600

    Login Logout Registration form created

--- classapp/urls.py
+++ classapp/urls.py
@@ -6,5 +6,9 @@ app_name = 'classapp'
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.PostDetailView, name='post_detail'),
+    path('dashboard/', views.user_dashboard_view, name='dashboard_redirect'),
+    path('registration/', views.user_registration_view, name='registration_redirect'),
+    path('login/', views.user_login_view, name='login_redirect'),
+    path('logout/', views.user_logout_view, name='logout_redirect')
 
 ]
--- classapp/views.py
+++ classapp/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from .models import User, Post
+from django.contrib.auth import login, logout
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import authenticate, AuthenticationForm, UserCreationForm
+from django.contrib import messages
+from .models import Post
 
 
 # Create your views here.
@@ -18,3 +19,58 @@ def PostDetailView(request, year, month, day, post):
                              published__day=day)
 
     return render(request, 'classapp/post_detail.html', {'post': post})
+
+
+def user_dashboard_view(request):
+    if request.user.is_authenticated:
+        return render(request, 'classapp/dashboard.html')
+    else:
+        return redirect('/login/')
+
+
+def user_registration_view(request):
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                new_user = authenticate(username=form.cleaned_data['username'],
+                                        password=form.cleaned_data['password1'])
+                login(request, new_user)
+                messages.add_message(request, messages.SUCCESS, f'Hurray! now {request.user} is registered user.')
+                return redirect('/dashboard/')
+        else:
+            form = UserCreationForm()
+        return render(request, 'classapp/registration.html', {'form': form})
+    else:
+        return redirect('/login/')
+
+
+def user_login_view(request):
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = AuthenticationForm(request=request, data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password']
+                user = authenticate(username=username, password=password)
+                login(request, user)
+                messages.add_message(request, messages.SUCCESS, f'{request.user} login successfully')
+                return redirect('/dashboard/')
+        else:
+            form = AuthenticationForm()
+        return render(request, 'classapp/login.html', {'form': form})
+    else:
+        return redirect('/login/')
+
+
+def user_logout_view(request):
+    if request.user.is_authenticated:
+        messages.add_message(request, messages.SUCCESS, f'{request.user} logout successfully. Continue with login.')
+        logout(request)
+        return redirect('/login/')
+    else:
+        return redirect('/login')
+
+
+
